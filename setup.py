09d309f32c2484cf5cf87e5a7d0a417978b52ddd
Josh Carter <105235743+joshcarter-cu@users.noreply.github.com>
2025-04-22 15:01:07 -0400

    fix(CP-39604): Initialize with version 0.0.0 (#10)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ AUTHOR = "Allard de Wit"
 AUTHOR_EMAIL = 'allard.dewit@wur.nl'
 URL = 'http://github.com/ajwdewit/pcse/'
 LICENSE="EUPL"
-VERSION = "1.0.0"
+VERSION = "0.0.0"
 
 here = os.path.abspath(os.path.dirname(__file__))
 
